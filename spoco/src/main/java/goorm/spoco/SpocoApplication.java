994aaf83c4ae9c0f688a3a926b6641d562d47cfa
githublees <leeho_@naver.com>
2024-06-07 03:58:19 +0900

    프로젝트 초기화 및 의존성 설정

---
+++ spoco/src/main/java/goorm/spoco/SpocoApplication.java
@@ -0,0 +1,13 @@
+package goorm.spoco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpocoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpocoApplication.class, args);
+	}
+
+}
---
+++ spoco/src/test/java/goorm/spoco/SpocoApplicationTests.java
@@ -0,0 +1,13 @@
+package goorm.spoco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
