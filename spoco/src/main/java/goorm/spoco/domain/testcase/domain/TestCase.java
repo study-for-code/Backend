678ec102ed9f465bd0508fd1305cedcac019c731
githublees <leeho_@naver.com>
2024-06-08 00:43:43 +0900

    TGST-166 feat: JPA 연관관계 추가

--- spoco/src/main/java/goorm/spoco/domain/testcase/domain/TestCase.java
+++ spoco/src/main/java/goorm/spoco/domain/testcase/domain/TestCase.java
@@ -1,5 +1,9 @@
 package goorm.spoco.domain.testcase.domain;
 
+import goorm.spoco.domain.algorithm.domain.Algorithm;
+import goorm.spoco.domain.category.domain.Category;
+import goorm.spoco.domain.study.domain.Study;
+import goorm.spoco.domain.subscribe.domain.Subscribe;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -14,6 +18,24 @@ public class TestCase {
     private String input;
     private String output;
 
+    @ManyToOne
+    @JoinColumn(name = "ALGORITHM_ID")
+    private Algorithm algorithm;
+
     public TestCase() {
+    }
+
+    //== 연관관계 메서드 ==//
+    public void addAlgorithm(Algorithm algorithm) {
+        this.algorithm = algorithm;
+        algorithm.getTestCases().add(this);
+    }
+
+    //== 생성 메서드 ==//
+    public static TestCase testCase(TestCase testCaseRequest) {
+        TestCase testCase = new TestCase();
+        testCase.input = testCaseRequest.input;
+        testCase.output = testCaseRequest.output;
+        return testCase;
     }
 }
