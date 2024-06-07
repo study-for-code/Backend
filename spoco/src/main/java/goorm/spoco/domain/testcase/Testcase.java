eef9f466e22218ffe90edce0539784d5fdd670ff
RealTone <realtone98@gmail.com>
2024-06-07 20:51:56 +0900

    TGST-168 Testcase entity 추가

--- spoco/src/main/java/goorm/spoco/domain/testcase/Testcase.java
+++ spoco/src/main/java/goorm/spoco/domain/testcase/Testcase.java
@@ -1,2 +1,19 @@
-package goorm.spoco.domain.testcase;public class Testcase {
+package goorm.spoco.domain.testcase;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Testcase {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "TESTCASE_ID")
+    private Long testcaseId;
+
+    private String input;
+    private String output;
+
+    public Testcase() {
+    }
 }
