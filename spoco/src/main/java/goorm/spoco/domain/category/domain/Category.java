f3d5950100e2720122bd39697283393bbabf4a38
githublees <leeho_@naver.com>
2024-06-07 21:36:46 +0900

    TGST-169 feat: Category 엔티티 추가

---
+++ spoco/src/main/java/goorm/spoco/domain/category/domain/Category.java
@@ -0,0 +1,19 @@
+package goorm.spoco.domain.category.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CATEGORY_ID")
+    private Long categoryId;
+
+    private String title;
+}
