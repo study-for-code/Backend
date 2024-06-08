5701c13e4877ac3a739f7bc35774383f566948a4
anothercod <tw19970520@gmail.com>
2024-06-08 18:01:17 +0900

    TGST-158 feat: @NotEmpty 추가

--- spoco/src/main/java/goorm/spoco/domain/member/domain/Member.java
+++ spoco/src/main/java/goorm/spoco/domain/member/domain/Member.java
@@ -1,6 +1,8 @@
 package goorm.spoco.domain.member.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,10 +17,14 @@ public class Member {
     @Column(name = "MEMBER_ID")
     private Long memberId;
 
+    @Email(message = "유효한 이메일을 입력하세요.")
+    @NotEmpty(message = "이메일은 필수 항목입니다.")
     private String email;
 
+    @NotEmpty(message = "사용자 이름은 필수 항목입니다.")
     private String nickname;
 
+    @NotEmpty(message = "비밀번호는 필수 항목입니다.")
     private String password;
 
     @Enumerated(EnumType.STRING)
