4962bfe0d551c88098375f7fa1549be1bf81150a
RealTone <realtone98@gmail.com>
2024-06-09 16:08:16 +0900

    TGST-63 feat: testcase update 기능 추가

--- spoco/src/main/java/goorm/spoco/domain/testcase/service/TestcaseService.java
+++ spoco/src/main/java/goorm/spoco/domain/testcase/service/TestcaseService.java
@@ -3,6 +3,7 @@ package goorm.spoco.domain.testcase.service;
 import goorm.spoco.domain.algorithm.domain.Algorithm;
 import goorm.spoco.domain.algorithm.repository.AlgorithmRepository;
 import goorm.spoco.domain.testcase.domain.Testcase;
+import goorm.spoco.domain.testcase.dto.TestcaseDTO;
 import goorm.spoco.domain.testcase.repository.TestcaseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -26,5 +27,15 @@ public class TestcaseService {
         Testcase testcase = testcaseRepository.findById(testcaseId)
                 .orElseThrow(() -> new IllegalArgumentException(testcaseId + "에 해당하는 테스트케이스가 존재하지 않습니다."));
         testcaseRepository.delete(testcase);
+    }
+
+    public Testcase update(Long testcaseId, TestcaseDTO testcaseDTO) {
+        Testcase testcase = testcaseRepository.findById(testcaseId)
+                .orElseThrow(() -> new IllegalArgumentException(testcaseId + "에 해당하는 테스트케이스가 존재하지 않습니다."));
+
+        testcase.setInput(testcaseDTO.getInput());
+        testcase.setOutput(testcaseDTO.getOutput());
+
+        return testcaseRepository.save(testcase);
     }
 }
