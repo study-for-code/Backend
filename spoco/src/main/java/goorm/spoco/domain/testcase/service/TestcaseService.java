428f2104f36acd86bcddeb460bbfb6585a6d2006
githublees <leeho_@naver.com>
2024-06-09 12:53:44 +0900

    TGST-177: refector: 트랜젝션 어노테이션 리포지토리 계층에서 서비스 계층으로 이동

--- spoco/src/main/java/goorm/spoco/domain/testcase/service/TestcaseService.java
+++ spoco/src/main/java/goorm/spoco/domain/testcase/service/TestcaseService.java
@@ -6,14 +6,17 @@ import goorm.spoco.domain.testcase.domain.Testcase;
 import goorm.spoco.domain.testcase.repository.TestcaseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class TestcaseService {
 
     private final TestcaseRepository testcaseRepository;
     private final AlgorithmRepository algorithmRepository;
 
+    @Transactional
     public Testcase save(Testcase testcase, Long algorithmId) {
         Algorithm algorithm = algorithmRepository.findById(algorithmId)
                 .orElseThrow(() -> new IllegalArgumentException(algorithmId + "에 해당하는 알고리즘이 존재하지 않습니다."));
