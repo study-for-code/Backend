81c2d90fa3a49abd401d7420a4b8edd980455c51
RealTone <realtone98@gmail.com>
2024-06-10 23:14:55 +0900

    TGST-30 feat: title로 알고리즘 리스트 찾는 메서드 생성

--- spoco/src/main/java/goorm/spoco/domain/algorithm/repository/AlgorithmRepository.java
+++ spoco/src/main/java/goorm/spoco/domain/algorithm/repository/AlgorithmRepository.java
@@ -5,10 +5,13 @@ import goorm.spoco.domain.algorithm.domain.AlgorithmStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AlgorithmRepository extends JpaRepository<Algorithm, Long> {
 
     Optional<Algorithm> findAlgorithmByAlgorithmIdAndAlgorithmStatus(Long id, AlgorithmStatus status);
+
+    List<Algorithm> findAlgorithmsByTitleLike(String title);
 }
