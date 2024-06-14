bac0b03701b4ded0df96e1375b460a9d6f85e47e
RealTone <realtone98@gmail.com>
2024-06-14 11:40:25 +0900

    TGST-91 feat: Dockerfile 추가

--- spoco/src/main/java/goorm/spoco/domain/member/config/MemberSecurityConfig.java
+++ spoco/src/main/java/goorm/spoco/domain/member/config/MemberSecurityConfig.java
@@ -1,53 +1,53 @@
-package goorm.spoco.domain.member.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-
-@Configuration
-@EnableWebSecurity
-public class MemberSecurityConfig {
-
-    //== H2 콘솔에 Security 등록 ==//
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
-                .csrf((csrf) -> csrf
-                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
-                .headers((headers) -> headers
-                        .addHeaderWriter(new XFrameOptionsHeaderWriter(
-                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
-                .formLogin((formLogin) -> formLogin
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/"))
-                .logout((logout) -> logout
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/")
-                .invalidateHttpSession(true));
-
-        return http.build();
-    }
-
-    //== password : password 를 빈등록 하여 객체 주입 ==//
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    //== 사용자 인증 : AuthenticationManager @Bean 등록 ==//
-    @Bean
-    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
-}
+//package goorm.spoco.domain.member.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+//import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+//
+//
+//@Configuration
+//@EnableWebSecurity
+//public class MemberSecurityConfig {
+//
+//    //== H2 콘솔에 Security 등록 ==//
+//    @Bean
+//    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+//                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+//                .csrf((csrf) -> csrf
+//                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+//                .headers((headers) -> headers
+//                        .addHeaderWriter(new XFrameOptionsHeaderWriter(
+//                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+//                .formLogin((formLogin) -> formLogin
+//                        .loginPage("/login")
+//                        .defaultSuccessUrl("/"))
+//                .logout((logout) -> logout
+//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//                .logoutSuccessUrl("/")
+//                .invalidateHttpSession(true));
+//
+//        return http.build();
+//    }
+//
+//    //== password : password 를 빈등록 하여 객체 주입 ==//
+//    @Bean
+//    PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    //== 사용자 인증 : AuthenticationManager @Bean 등록 ==//
+//    @Bean
+//    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+//        return authenticationConfiguration.getAuthenticationManager();
+//    }
+//}
--- spoco/src/main/java/goorm/spoco/domain/member/service/MemberService.java
+++ spoco/src/main/java/goorm/spoco/domain/member/service/MemberService.java
@@ -5,7 +5,7 @@ import goorm.spoco.domain.member.domain.Member;
 import goorm.spoco.domain.member.repository.MemberRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -16,14 +16,14 @@ import org.springframework.stereotype.Service;
 public class MemberService {
 
     private final MemberRepository memberRepository;
-    private final PasswordEncoder passwordEncoder;
+//    private final PasswordEncoder passwordEncoder;
 
     public MemberDTO create(String nickname, String email, String password) {
         // 생성을 Create 로 사용해도 되는지
         MemberDTO user = new MemberDTO();
         user.setNickname(nickname);
         user.setEmail(email);
-        user.setPassword(passwordEncoder.encode(password));
+//        user.setPassword(passwordEncoder.encode(password));
 //        this.memberRepository.save(user); // => 이부분 어떻게 처리 해야되는지 여쭤보기
         return user;
     }
